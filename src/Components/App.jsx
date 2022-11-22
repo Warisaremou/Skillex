8efc9d02f81a2c7f09893c22d82bf0aeaba5c0ee
warisaremou <warisaremou2003@gmail.com>
2022-11-22 08:39:43 +0100

    Setting up my project with react and tailwindcss

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-blue-500 font-bold">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
