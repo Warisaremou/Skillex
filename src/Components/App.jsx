d9043a271fb8a7a04b7dc0dfa92c4c8bf68056a8
warisaremou <warisaremou2003@gmail.com>
2022-12-12 10:42:32 +0100

    Adding useState to toggle menu

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,13 @@
 import NavBar from "./NavBar/NavBar";
+import { useState } from "react";
 
 function App() {
+
+  const [openMenu, setIsOpen] = useState(false);
+
   return (
     <div className="container font-poppins">
-     <NavBar />
+      <NavBar openMenu={openMenu} setIsOpen={setIsOpen} />
     </div>
   );
 }
