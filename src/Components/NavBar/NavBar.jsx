cb60dd7719e9a566aa7391ee464eea27af71921f
warisaremou <warisaremou2003@gmail.com>
2022-12-12 10:43:42 +0100

    Importing icons from react-icons to toggle menu

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,25 +1,48 @@
 import React from "react";
+import { CgMenuRightAlt, CgClose } from "react-icons/cg";
 
-function NavBar() {
+function NavBar({ openMenu, setIsOpen }) {
     return (
         <nav className="nav-bar">
-            <span className="logo">Skillex <span className="text-green-500">.</span></span>
-            <div className="menu">
+            <span className="logo">
+                Skillex <span className="text-green-500">.</span>
+            </span>
+            <CgMenuRightAlt
+                className="toogle-menu"
+                onClick={() => setIsOpen(true)}
+            />
+            <div
+                className={`menu ${
+                    openMenu ? "left-0 right-0" : "-left-full lg:left-0"
+                }`}
+            >
+                <CgClose
+                    className="toogle-menu close"
+                    onClick={() => setIsOpen(false)}
+                />
                 <ul className="nav-menu">
                     <li className="nav-item">
-                        <a className="nav-link active" href="/">Find passion</a>
+                        <a className="nav-link active" href="/">
+                            Find passion
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Categories</a>
+                        <a className="nav-link" href="/">
+                            Categories
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Skills</a>
+                        <a className="nav-link" href="/">
+                            Skills
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Customer</a>
+                        <a className="nav-link" href="/">
+                            Customer
+                        </a>
                     </li>
                 </ul>
-                <div className="">
+                <div>
                     <button className="btn">Login</button>
                     <button className="btn btn-primary">Free Trial</button>
                 </div>
